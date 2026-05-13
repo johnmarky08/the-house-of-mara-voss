179dd4186f082ecfa38959b0d1c06a91157d78c7
Josh45645 <bartolomej863@gmail.com>
2026-05-13 09:26:45 +0800

    update

--- scripts/ui/RightArrow.cs
+++ scripts/ui/RightArrow.cs
@@ -1,24 +1,25 @@
 using Godot;
 
-public partial class RightArrow : Sprite2D
+public partial class RightArrow : Area2D
 {
-    /* public override void _Input(InputEvent @event)
+    public override void _Ready()
     {
-        if (!Visible)
-            return;
+        InputPickable = true;
+    }
 
+    public override async void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
+    {
         if (@event is not InputEventMouseButton mouseEvent)
             return;
 
         if (!mouseEvent.Pressed || mouseEvent.ButtonIndex != MouseButton.Left)
             return;
 
-        Globals.Instance.SHARDS_COLLECTED[0] = true;
-        GetTree().ProcessFrame += async () =>
-        {
-            await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
-            SceneManager.ChangeScene("res://scenes/world/room_2.tscn");
-        };
-    } */
+        if (Globals.Instance != null)
+            Globals.Instance.SHARDS_COLLECTED[0] = true;
 
+        await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+
+        SceneManager.ChangeScene("res://scenes/world/room_2.tscn");
+    }
 }
